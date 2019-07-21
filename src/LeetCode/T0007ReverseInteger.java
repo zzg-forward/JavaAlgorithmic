84ee5ae160869c25ae3d0b4eb6e6be9ab045dd45
zzg-forward <1198952476@qq.com>
2019-07-21 16:35:01 +0800

    第7道题

--- src/LeetCode/T0007ReverseInteger.java
+++ src/LeetCode/T0007ReverseInteger.java
@@ -1,11 +1,30 @@
 package LeetCode;
 
+/**
+ * Reverse Integer
+ *
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * 题目：https://leetcode.com/problems/reverse-integer/submissions/
+ *
+ * 解法：就是翻转数字，运用取余，进位的做法
+ */
+
 public class T0007ReverseInteger {
     public static void main(String[] args) {
-        getReverseInteger(123);
+        System.out.println(reverseInteger(-123));
     }
 
-    private static int getReverseInteger(int target) {
-        return target + 1;
+    private static int reverseInteger(int target) {
+        int result = 0;
+        while (target != 0) {
+            if (Math.abs(result) > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            result = result * 10 + target % 10;
+            target = target / 10;
+        }
+        return result;
+
     }
 }
